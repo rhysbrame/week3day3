e905ac803180dce99c525220e825ed60659a1abf
Rhys Brame <rhysbrame@gmail.com>
2017-03-23 08:21:31 +0000

    added methods update self.find to Album class

--- db/console.rb
+++ db/console.rb
@@ -3,6 +3,7 @@ require_relative('../models/album')
 require_relative('../models/artist')
 
 Artist.delete_all()
+Album.delete_all()
 
 artist1 = Artist.new({'name' => 'Ryan Adams'})
 artist2 = Artist.new({'name' => 'The Shins'})
@@ -13,8 +14,8 @@ album2 = Album.new({'title' => 'Heartbreaker', 'genre' => 'rock'})
 artist1.save()
 artist2.save()
 
-album1.save()
-album2.save()
+# album1.save()
+# album2.save()
 
 binding.pry
 nil
--- models/album.rb
+++ models/album.rb
@@ -7,6 +7,7 @@ class Album
     @title = options['title']
     @genre = options['genre']
     @id = options['id'].to_i if options['id']
+    @artist_id = options['artist_id'].to_i
   end
 
   def save
@@ -15,11 +16,34 @@ class Album
     @id = result[0]['id'].to_i
   end
 
+  def update
+    sql = "UPDATE albums SET (title, genre, artist_id) = ('#{@title}', '#{@genre}', #{@artist_id}) WHERE id = #{@id}"
+    result =  SqlRunner.run(sql)
+    return result
+  end
 
+  def self.find(id)
+    sql = "SELECT * FROM artist WHERE id = #{id}"
+    result = SqlRunner.run(sql)
+  end
 
+  def self.all
+    sql = "SELECT * FROM albums"
+    result = SqlRunner.run(sql)
+    album_info = result.map {|album| Album.new(album)}
+    return album_info 
+  end
 
+  def self.delete_all
+    sql = "DELETE FROM albums"
+    result = SqlRunner.run(sql)
+    return result
+  end
 
-
-
+  def delete
+    sql = "DELETE FROM albums WHERE id = #{@id}"
+    result = SqlRunner.run(sql)
+    return result
+  end
 
 end
